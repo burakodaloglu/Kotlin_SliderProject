85fd908a411ff8be8e5ee65e55b074ba1ada2b97
Burak KODALOĞLU <burakkodaloglu1@gmail.com>
2022-09-13 17:32:56 +0300

    Kotlin

---
+++ app/src/main/java/com/burakkodaloglu/sliderproject/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.burakkodaloglu.sliderproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import java.lang.Math.abs
+
+private lateinit var viewPager2:ViewPager2
+private lateinit var handler:Handler
+private lateinit var imageList:ArrayList<Int>
+private lateinit var adapter: ImageAdapter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+        setUpTransformer()
+        viewPager2.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable,5000)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable,5000)
+    }
+
+    private val  runnable=Runnable{
+        viewPager2.currentItem= viewPager2.currentItem+1
+    }
+    private fun setUpTransformer(){
+        val transformer=CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer{page,position->
+            val r =1 - abs(position)
+            page.scaleY=0f + r+0f
+        }
+        viewPager2.setPageTransformer(transformer)
+    }
+    private fun init(){
+
+        viewPager2=findViewById(R.id.viewPager2)
+        handler=Handler(Looper.myLooper()!!)
+        imageList= ArrayList()
+
+        imageList.add(R.drawable.slider1)
+        imageList.add(R.drawable.slider2)
+        imageList.add(R.drawable.slider3)
+        imageList.add(R.drawable.slider4)
+        imageList.add(R.drawable.slider5)
+
+
+        adapter= ImageAdapter(imageList, viewPager2)
+        viewPager2.adapter= adapter
+        viewPager2.offscreenPageLimit=3
+        viewPager2.clipToPadding=false
+        viewPager2.clipChildren=false
+        viewPager2.getChildAt(0).overScrollMode=RecyclerView.OVER_SCROLL_NEVER
+    }
+}
